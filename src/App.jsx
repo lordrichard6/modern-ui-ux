9581c944c8dea707479c813b39afe8f98919bbe4
lordrichard6 <paulolopesreizinho@gmail.com>
2022-11-16 18:18:44 +0100

    chore:creating componets and containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+
+import { Footer, Blog, Possibility, WhatGPT3, Header, Features } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-        <h1>APPS</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
